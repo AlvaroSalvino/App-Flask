061454c9466d9c83647c2bd00856154c774992ea
Alvaro Salvino <alvaro.salvino@gmail.com>
2024-01-07 15:11:57 -0300

    Api de Precificação

--- api-flask.py
+++ api-flask.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+itens = [
+    {
+        "nome": "Teclado",
+        "Preco": 25.90
+    },
+    {
+        "nome": "Mouse",
+        "Preco": 20.88
+    },
+    {
+        "nome": "Cadeira Gamer",
+        "Preco": 2000.00
+    }
+]
+
 @app.route("/")
 def motraItens():
-    return "Itens do banco de dados"
+    return jsonify(itens)
+@app.route("/itens/<int:id>")
+def mostrarIntesPorId(id):
+    return(jsonify(itens[id]))
 
 if __name__=="__main__":
     app.run(debug= True)
